2612476fa0a7c6b66a0da5aa786daabd17a24f1c
Trevor Davis <tmasondavis@yahoo.com>
2025-12-09 14:36:55 -0500

    updated databse setting to allow to run supabase database

--- pit_portal/settings.py
+++ pit_portal/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -95,10 +96,7 @@ ASGI_APPLICATION = 'pit_portal.asgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 AUTH_USER_MODEL = "accounts.CustomUser"
