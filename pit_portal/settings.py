8e32102115b6754bbdd3535805ca654113b5875c
Trevor Davis <tmasondavis@yahoo.com>
2025-12-09 12:11:05 -0500

    updated settings with allowed csrf token site

--- pit_portal/settings.py
+++ pit_portal/settings.py
@@ -30,6 +30,8 @@ DEBUG = True
 # otherwise: use '*' for testing
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'pit-portal.fly.dev']
 
+CSRF_TRUSTED_ORIGINS = ['https://pit-portal.fly.dev']
+
 
 # Application definition
 
