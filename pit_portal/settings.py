09d50f8a910bc816916f20239db282efeb3a9d08
Trevor Davis <tmasondavis@yahoo.com>
2025-12-11 14:40:31 -0500

    updated channel layer setting with fallback

--- pit_portal/settings.py
+++ pit_portal/settings.py
@@ -168,7 +168,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [os.environ["REDIS_URL"]],
+            "hosts": [os.environ.get("REDIS_URL", "redis://localhost:6379")],
         },
     },
 }
